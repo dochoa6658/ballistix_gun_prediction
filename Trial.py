323550c901f3494d3d02d946a678ae8d1c2a0ac9
Daniel Ochoa <dochoa6658@gmail.com>
2020-02-17 16:49:06 -0600

    update

--- Trial.py
+++ Trial.py
@@ -1,8 +1,21 @@
 import gun_prediction
+import sys
+import importlib
 
 counts = dict()
-print(gun_prediction.gun_name)
-
-counts[tuple(gun_prediction.gun_name)] = 1
-
+# print(gun_prediction.gun_name)
+for x in range(100):
+    if tuple(gun_prediction.gun_name) in counts:
+        counts[tuple(gun_prediction.gun_name)] += 1
+    else:
+        counts[tuple(gun_prediction.gun_name)] = 1
+    importlib.reload(gun_prediction)
 print(counts)
+
+
+
+# for word in words:
+#         if word in counts:
+#             counts[word] += 1
+#         else:
+#             counts[word] = 1
